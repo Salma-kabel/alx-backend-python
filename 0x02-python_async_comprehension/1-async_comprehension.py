d7e00d620b94d93ad061f1a2e997ae5873f9fb94
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 18:41:58 +0000

    task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,4 +2,13 @@
 """coroutine called async_comprehension that takes no arguments"""
 
 
+from typing import List
+from random import uniform
+import asyncio
 
+
+async def async_comprehension() -> List[float]:
+    """will collect 10 random numbers using an async comprehensing over
+    async_generator, then return the 10 random numbers"""
+    res = [await i async for i in async_generator()]
+    return res
