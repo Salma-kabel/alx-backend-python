ca1c0037b1d8f64480a243d7f19f28678f174f88
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 20:18:08 +0000

    task2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,6 +11,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """spawn wait_random n times with the specified max_delay"""
     time = [wait_random(max_delay) for i in range(n)]
     results = await asyncio.gather(*time)
     return sorted(results)
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""function with integers n and max_delay as arguments that
+measures the total execution time for wait_n(n, max_delay)"""
+
+
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """measures the total execution time for
+    wait_n(n, max_delay), and returns total_time / n"""
+    begin = time.time()
+    wait_n(n, max_delay)
+    end = time.time()
+    total = end - start
+    avrg = total / n
+    return avrg
