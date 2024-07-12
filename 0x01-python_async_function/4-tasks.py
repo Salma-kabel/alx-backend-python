81b311f7ac3219bc5d8f3bcef68567b363c36814
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 21:05:21 +0000

    task4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """like wait_n except task_wait_random is being called""" 
+    """like wait_n except task_wait_random is being called"""
     delay: List = []
 
     for i in range(n):
